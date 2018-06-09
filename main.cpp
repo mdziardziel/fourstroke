0115f1d68af7c8e426d9620185c61a8a2eb8fa52
mdziardziel <mdziardziel@gmail.com>
2018-06-09 09:01:23 +0200

    tekstury

--- main.cpp
+++ main.cpp
@@ -29,6 +29,7 @@ Place, Fifth Floor, Boston, MA  02110 - 1301  USA
 #include "constants.hpp"
 #include "class_template.hpp"
 #include <iostream>
+#include "lodepng.h"
 
 using namespace glm;
 
@@ -36,6 +37,7 @@ using namespace glm;
 float aspect=1;
 float speed=3.14; //prêdkoœæ k¹towa obrotu w radianach na sekundê
 float speed_x = 0, speed_y = 0;
+GLuint tex; //Globalnie
 
 //deklarujemy obiekty Template
 Template *mushroom;
@@ -99,34 +101,23 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 
 void move_circle(float &x, float &y, bool &course_x, bool &course_y, bool &valve_move, float &st360){
-    float spd = 0.05;
-    if(course_x){
-        x = x+ spd;
-    }else{
-        x = x - spd;
-    }
+    float spd = 3;
+    float promien = 0.12;
 
-    if(course_y){
-        y = y+ spd;
-    }else{
-        y = y - spd;
-    }
+    st360 = st360 + spd;
+    if(st360>=360)
+        st360 = 0;
 
-    //std::cout<<x<<" "<<y<<std::endl;
-    if(x >= 1.0)
-        course_x = false;
-    if(x <= -1.0)
-        course_x = true;
-
-    if(y >= 1.0)
-        course_y = false;
-    if(y <= -1.0){
-        course_y = true;
+    if(st360==90)
         valve_move = !valve_move;
-    }
 
-    st360 = st360 + spd*1.55;
-    if(st360>=360) st360 = 0;
+    y = std::sin(st360*PI/180)*promien;
+    if(st360!=90)x = std::cos(st360*PI/180)*promien;
+    else x = 0;
+
+    //std::cout<<valve_move<<" "<<st360<<" "<<x<<" "<<y<<std::endl;
+
+
 }
 
 
@@ -142,7 +133,24 @@ void initOpenGLProgram(GLFWwindow* window) {
     glEnable(GL_LIGHTING); //W³¹cz cieniowanie
     glEnable(GL_LIGHT0); //W³¹cz œwia³o numer 0
     glEnable(GL_DEPTH_TEST); //W³¹cz bufor g³êbokoœci
-    glEnable(GL_COLOR_MATERIAL); //W³¹cz ustawianie koloru materia³u przez polecenia glColor
+    //glEnable(GL_COLOR_MATERIAL); //W³¹cz ustawianie koloru materia³u przez polecenia glColor
+    glEnable(GL_NORMALIZE);
+    std::vector<unsigned char> image;   //Alokuj wektor do wczytania obrazka
+    unsigned width, height;   //Zmienne do których wczytamy wymiary obrazka
+    //Wczytaj obrazek
+    unsigned error = lodepng::decode(image, width, height, "metal.png");
+
+    //Import do pamięci karty graficznej
+    glGenTextures(1,&tex); //Zainicjuj jeden uchwyt
+    glBindTexture(GL_TEXTURE_2D, tex); //Uaktywnij uchwyt
+    //Wczytaj obrazek do pamięci KG skojarzonej z uchwytem
+    glTexImage2D(GL_TEXTURE_2D, 0, 4, width, height, 0,
+    GL_RGBA, GL_UNSIGNED_BYTE, (unsigned char*) image.data());
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glEnable(GL_TEXTURE_2D);
 }
 
 
@@ -167,14 +175,16 @@ void drawScene(GLFWwindow* window,float angle_x, float angle_y, float *dane_f, b
 
 
     //rysowanie prętu
+    glBindTexture(GL_TEXTURE_2D,tex);
     glColor4f(0,1,1,1);
     mat4 M,R,T,S,I,R2;
     I = mat4(1);
-    T = translate(I,vec3(dane_f[0]*0.2,dane_f[1]*0.2 -1,0));
+    T = translate(I,vec3(dane_f[0],dane_f[1] -1,0));
     S = scale(I,vec3(0.2,0.2,0.2));
     R=rotate(I,angle_x,vec3(1.0f,0.0f,0.0f));
 	R=rotate(R,-angle_y,vec3(0.0f,1.0f,0.0f));
-	R=rotate(R,dane_f[0]/3,vec3(0,0,1));
+	R=rotate(R,dane_f[0],vec3(0,0,1));
+	//std::cout<<dane_f[0]<<" "<<dane_f[1]<<std::endl;
 	M=R*T*S;
 
 	glLoadMatrixf(value_ptr(V*M)); //Za³aduj macierz model-widok
@@ -185,7 +195,7 @@ void drawScene(GLFWwindow* window,float angle_x, float angle_y, float *dane_f, b
 	//rysowanie tłoka
 	glColor4f(1,0,1,1);
     I = mat4(1);
-    T = translate(I,vec3(0,dane_f[1]*0.2 + 0.4,0));
+    T = translate(I,vec3(0,dane_f[1] + 0.4,0));
     S = scale(I,vec3(0.2,0.2,0.2));
     R=rotate(I,angle_x,vec3(1.0f,0.0f,0.0f));
 	R=rotate(R,-angle_y,vec3(0.0f,1.0f,0.0f));
@@ -211,8 +221,8 @@ void drawScene(GLFWwindow* window,float angle_x, float angle_y, float *dane_f, b
     //rysowanie zaworu po prawej
     glColor4f(1,1,0,1);
     I = mat4(1);
-    if(dane_b[2] && dane_f[0] >= 0)
-        if(dane_f[1]<=0)
+    if(dane_b[2] && dane_f[0] < 0)
+        if(dane_f[1]<0)
             T = translate(I,vec3((dane_f[0])*0.025+0.15,-(dane_f[1]+1)*0.05 + 1.03,0));
         else
             T = translate(I,vec3((dane_f[0])*0.025+0.15,(dane_f[1]-1)*0.05 + 1.03,0));
@@ -230,8 +240,8 @@ void drawScene(GLFWwindow* window,float angle_x, float angle_y, float *dane_f, b
 	//rysowanie zaworu po lewej
 	glColor4f(1,1,0,1);
     I = mat4(1);
-    if(dane_b[2] && dane_f[0] <= 0)
-        if(dane_f[1]<=0)
+    if(dane_b[2] && dane_f[0] > 0)
+        if(dane_f[1]<0)
             T = translate(I,vec3((dane_f[0])*0.025-0.12,-(dane_f[1]+1)*0.05 + 1.03,0));
         else
             T = translate(I,vec3((dane_f[0])*0.025-0.12,(dane_f[1]-1)*0.05 + 1.03,0));
@@ -251,12 +261,11 @@ void drawScene(GLFWwindow* window,float angle_x, float angle_y, float *dane_f, b
     glColor4f(1,0,0,1);
     I = mat4(1);
     T = translate(I,vec3(0.2,-0.75,0));
-    S = scale(I,vec3(0.15,0.15,0.15));
+    S = scale(I,vec3(0.15,0.1,0.15));
     R=rotate(I,angle_x,vec3(1.0f,0.0f,0.0f));
 	R=rotate(R,-angle_y,vec3(0.0f,1.0f,0.0f));
 	R=rotate(R,90*PI/180,vec3(0,1,0.0));
-	R2=rotate(I,-dane_f[2] + 90,vec3(1,0,0));
-	std::cout<<dane_f[2]<<std::endl;
+	R2=rotate(I,(-dane_f[2] +90)*PI/180 ,vec3(1,0,0));
 	M=R*T*R2*S;
 
 	glLoadMatrixf(value_ptr(V*M)); //Za³aduj macierz model-widok
@@ -325,7 +334,7 @@ int main(void)
 		drawScene(window,angle_x,angle_y, dane_f, dane_b); //Wykonaj procedurę rysującą
 		glfwPollEvents(); //Wykonaj procedury callback w zalezności od zdarzeń jakie zaszły.
 	}
-
+    glDeleteTextures(1,&tex);
 	glfwDestroyWindow(window); //Usuñ kontekst OpenGL i okno
 	glfwTerminate(); //Zwolnij zasoby zajête przez GLFW
 	exit(EXIT_SUCCESS);
